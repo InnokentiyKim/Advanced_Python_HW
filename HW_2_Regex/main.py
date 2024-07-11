1b16b5989589eafb7e0f524d91b993f6c92289b1
InnCent <innokentiykim90@gmail.com>
2024-07-11 21:10:38 +0500

    Bugs fixed

--- HW_2_Regex/main.py
+++ HW_2_Regex/main.py
@@ -1,6 +1,5 @@
 import re
 import csv
-from pprint import pprint
 
 
 def standardize_phone(phone_num: str) -> str:
@@ -14,29 +13,41 @@ def standardize_phone(phone_num: str) -> str:
     return res_number
 
 
+def combine_persons(contact: list, uniq_persons: dict) -> None:
+    lastname, name = [value.strip().lower() for value in contact[:2]]
+    person = tuple([lastname, name])
+    if person not in uniq_persons:
+        uniq_persons[person] = contact
+    else:
+        for ind, item in enumerate(contact):
+            if not uniq_persons[person][ind]:
+                uniq_persons[person][ind] = item
+
+
 def main():
     with open('phonebook_raw.csv', 'r') as file:
         reader = csv.reader(file, delimiter=',')
         contacts_list = list(reader)
     title = contacts_list[0]
     edited_contacts_list = []
-    uniq_person_dict = {}
+    uniq_persons_long_dict = {}
+    uniq_persons_short_dict = {}
     for contact in contacts_list[1:]:
         splitted_name = ' '.join(contact[0:3]).split()
-        contact[0:3] = splitted_name
+        for i in range(len(splitted_name)):
+            contact[i] = splitted_name[i]
         cur_phone = str(contact[-2])
+        surname = contact[3]
         contact[-2] = standardize_phone(cur_phone)
-        person = tuple(contact[:2])
-        if person not in uniq_person_dict:
-            uniq_person_dict[person] = contact
+        if surname:
+            combine_persons(contact, uniq_persons_long_dict)
         else:
-            for ind, item in enumerate(contact):
-                if not uniq_person_dict[person][ind]:
-                    uniq_person_dict[person][ind] = item
+            combine_persons(contact, uniq_persons_short_dict)
+    for contact in uniq_persons_short_dict.values():
+        combine_persons(contact, uniq_persons_long_dict)
     edited_contacts_list.append(title)
-    for con in uniq_person_dict.values():
+    for con in uniq_persons_long_dict.values():
         edited_contacts_list.append(con)
-    pprint(edited_contacts_list)
     with open('phonebook.csv', 'w', encoding='UTF-8') as file:
         writer = csv.writer(file, delimiter=',')
         writer.writerows(edited_contacts_list)
