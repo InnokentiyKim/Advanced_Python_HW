d98a2acfc841acd4ab8075d87c8614a21186c2eb
InnCent <innokentiykim90@gmail.com>
2024-07-29 23:33:33 +0500

    HW_6 added

---
+++ HW_6_Test_development/tests/test_yadisk_api.py
@@ -0,0 +1,35 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class TestYandexDisk:
+    def setup_method(self):
+        self.TOKEN = os.getenv("TOKEN")
+        self.url = "https://cloud-api.yandex.net/v1/disk/resources"
+        self.false_url = "https://cloud-api.yandex.net/v1/disk/esource"
+        self.params = {'path': 'Pictures'}
+        self.headers = {'Authorization': f"OAuth {self.TOKEN}"}
+        self.false_headers = {'Authorization': "OAuth"}
+
+    def teardown_method(self):
+        requests.delete(self.url, params=self.params, headers=self.headers)
+
+    def test_create_folder_201(self):
+        response = requests.put(self.url, params=self.params, headers=self.headers)
+        assert response.status_code == 201
+
+    def test_create_folder_401(self):
+        response = requests.put(self.url, params=self.params, headers=self.false_headers)
+        assert response.status_code == 401
+
+    def test_create_folder_404(self):
+        response = requests.put(self.false_url, params=self.params, headers=self.headers)
+        assert response.status_code == 404
+
+    def test_create_folder_409(self):
+        requests.put(self.url, params=self.params, headers=self.headers)
+        second_response = requests.put(self.url, params=self.params, headers=self.headers)
+        assert second_response.status_code == 409
---
+++ HW_6_Test_development/tests/test_yadisk_using_params.py
@@ -0,0 +1,27 @@
+import os
+import pytest
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+url = "https://cloud-api.yandex.net/v1/disk/resources"
+false_url = "https://cloud-api.yandex.net/v1/disk/esource"
+params = {'path': 'Pictures'}
+headers = {'Authorization': f"OAuth {TOKEN}"}
+false_headers = {'Authorization': "OAuth"}
+
+
+@pytest.mark.parametrize(
+    'url,params,headers,status_code',
+    [
+        (url, params, headers, 201),
+        (url, params, false_headers, 401),
+        (false_url, params, headers, 404),
+        (url, params, headers, 409),
+    ]
+)
+def test_create_folder(url, params, headers, status_code):
+    response = requests.put(url, params=params, headers=headers)
+    assert response.status_code == status_code
